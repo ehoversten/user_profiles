e2aa65a689e24d076939eeead02ea6d9e498a167
ehoversten <ehoversten@gmail.com>
2018-10-26 18:10:21 -0400

    added accounts app, base.html, auth.view login view

---
+++ accounts/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from django.contrib.auth.views import login
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^login/$', login, {'template_name': 'accounts/login.html'})
+]
