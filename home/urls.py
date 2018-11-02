d08155db9fc77e49a74df6492c65ff4f086fdf2d
ehoversten <ehoversten@gmail.com>
2018-11-02 15:45:39 -0400

    added class based view to home app

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
-from . import views
+# from . import views
+from home.views import HomeView
 
 app_name = 'home'
 
 urlpatterns = [
-    url(r'^$', views.dashboard, name='dashboard'),
+    # url(r'^$', views.dashboard, name='dashboard'),
+    url(r'^$', HomeView.as_view(), name='dashboard'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect
-
+from django.views.generic import TemplateView
 # Create your views here.
-def dashboard(request):
-    context = {
+# def dashboard(request):
+#     context = {
+#
+#     }
+#     return render(request, 'home/dashboard.html', context)
 
-    }
-    return render(request, 'home/dashboard.html', context)
+
+class HomeView(TemplateView):
+    template_name = 'home/dashboard.html'
