9b41136e2673d863a13de7d548c15673e51a85a2
ehoversten <ehoversten@gmail.com>
2018-11-05 22:06:16 -0500

    added form submit to view

--- home/views.py
+++ home/views.py
@@ -1,12 +1,32 @@
-from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+from home.forms import HomeForm
 # Create your views here.
-# def dashboard(request):
-#     context = {
-#
-#     }
-#     return render(request, 'home/dashboard.html', context)
 
 
 class HomeView(TemplateView):
     template_name = 'home/dashboard.html'
+
+
+    def get(self, request):
+        # initalize form instance
+        form = HomeForm()
+        context = {
+            'form' : form
+
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        form = HomeForm(request.POST)
+        if form.is_valid():
+            text = form.cleaned_data['post']
+            print(text)
+            form = HomeForm()
+            
+        context = {
+            'form' : form,
+            'text' : text,
+
+        }
+        return render(request, self.template_name, context)
